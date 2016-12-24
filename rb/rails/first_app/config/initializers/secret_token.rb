75214e5207ff0ebb1080d9c4892b747d51786e55
Carlos Duque <carlosduque.hn@gmail.com>
2016-12-24 15:24:16 -0300

    removed first_app from rails

--- rb/rails/first_app/config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-FirstApp::Application.config.secret_token = '655c7c4d712b550bd60b35c639597a36edb55b730985bc86a53be8c20ba906c923cd7a7f114dca718c1efa310734c182d8470d33dc3111f5ebb99f5f7898668c'
