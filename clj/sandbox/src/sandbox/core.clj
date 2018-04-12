d17a90a04feae6036b022f89efe08d6b2169c145
Carlos Duque <carlosduque.hn@gmail.com>
2018-04-12 17:37:06 -0300

    added collection examples and a few functional techniques

--- clj/sandbox/src/sandbox/core.clj
+++ clj/sandbox/src/sandbox/core.clj
@@ -175,3 +175,203 @@
 ;regex
 (re-seq #"\w+"     "one-two/three")
 (re-seq #"\w*(\w)" "one-two/three")
+
+;collection types
+(def ds [:willie :barnabas :adam])
+ds
+
+(def ds1 (replace {:barnabas :quentin} ds))
+ds1
+ds
+
+(class (hash-map :a 1))
+(seq (hash-map :a 1))
+(class (seq (hash-map :a 1)))
+(seq (keys (hash-map :a 1)))
+(class (keys (hash-map :a 1)))
+
+(vec (range 10))
+
+(let [my-vector [:a :b :c]]
+  (into my-vector (range 10)))
+
+(into (vector-of :int) [Math/PI 2 1.3])
+(into (vector-of :char) [100 101 102])
+
+(def a-to-j (vec (map char (range 65 75 ))))
+a-to-j
+
+(nth a-to-j 4)
+(get a-to-j 4)
+(a-to-j 4)
+(seq a-to-j)
+(rseq a-to-j)
+(assoc a-to-j 4 "no longer E")
+
+(replace {2 :a 4 :b} [1 2 3 2 3 4])
+
+(def matrix
+  [[1 2 3]
+   [4 5 6]
+   [7 8 9]])
+(get-in matrix [1 2])
+(assoc-in matri [1 2] 'x)
+(update-in matrix [1 2] * 100)
+
+(defn neighbors
+  ([size yx] (neighbors [[-1 0] [1 0] [0 -1] [0 1]]
+                        size
+                        yx))
+  ([deltas size yx]
+   (filter (fn [new-yx]
+             (every? #(< -1 % size) new-x))
+           (map #(vec (map + yx %))
+                deltas))))
+
+(neighbors 3 [0 0])
+
+;; vectors as stacks
+(def my-stack [1 2 3])
+(peek my-stack)
+(pop my-stack)
+(conj my-stack 4)
+(+ (peek my-stack) (peek (pop my-stack)))
+
+(subvec a-to-j 3 6)
+
+(first {:width 10 :height 20 :depth 15})
+(vector? (first {:width 10 :height 20 :depth 15}))
+
+(doseq [[dimenson amount] {:width 10 :height 20 :depth 15}]
+  (println (str (name dimension) ":") amount "inches"))
+
+;; lists
+(cons 1 '(2 3))
+(conj '(2 3) 1)
+
+;;queues
+;;; first provide a multimethod for pretty printing
+(defmethod print-method clojure.lang.PersistentQueue
+  [q w]
+  (print-method '<- w)
+  (print-method (seq q) w)
+  (print-method '-< w))
+
+(def schedule
+  (conj clojure.lang.PersintentQueue/EMPTY
+        :wake-up :shower :brush-teeth))
+
+(peek schedule)
+(pop schedule)
+(rest schedule)
+
+;; sets
+(#{:a :b :c :d} :c)
+
+(#{:a :b :c :d} :e)
+
+(get #{:a 1 :b 2} :b)
+(get #{:a 1 :b 2} :z :nothing-doing)
+
+(into #{[]} [()])
+(into #{[1 2]} '[(1 2)])
+
+(some #{:b} [:a 1 :b 2])
+(some #{1 :b} [:a 1 :b 2])
+
+(sorted-set :b :c :a)
+(sorted-set [3 4] [1 2])
+
+(require 'clojure.set))
+;(ns my.cool.lib
+;  (:require clojure.set))
+
+(clojure.set/intersection #{:humans :fruit-bats :zombies}
+                          #{:chupacabra :zombies :humans})
+
+(clojure.set/intersection #{:pez :gum :dots :skor}
+                          #{:pez :skor :pocky}
+                          #{:pocky :gum :skor})
+
+(clojure.set/union #{:humans :fruit-bats :zombies}
+                   #{:chupacabra :zoies :humans})
+
+(clojure.set/union #{:pez :gum :dots :skor}
+                   #{:pez :skor :pocky}
+                   #{:pocky :gum :skor})
+
+(clojere.set/difference #{1 2 3 4} #{3 4 5 6})
+
+;;maps
+(hash-map :a 1, :b 2, :c 3, :d 4, :e )
+(let [m {:a 1 1 :b [1 2 3] "4 5 6"}]
+  [(get m :a) (get m [1 2 3])])
+
+(seq {:a 1 :b 2})
+(into {} (map vec '[(:a 1) (:b 2)]))
+
+(apply hash-map [:a 1 :b 2])
+(zipmap [:a :b] [1 2])
+
+(sorted-map :thx 1138 :r2d 2)
+(sorted-map "bac" 2 "abc" 9)
+(sorted-map-by #(compare (subs 1 1) (sus %2 1)) "bac" 2 "abc" 9)
+
+(assoc {1 :int} 1.0 :float)
+
+;;keeping order
+(seq (hash-map :a 1 : 2 :c 3))
+(seq (array-map :a 1 : 2 :c 3))
+
+(defn index [coll]
+  (cond
+    (map? coll) (seq coll)
+    (set? coll) (map vector coll coll)
+    :else (map vector (iterate inc 0) coll)))
+
+(index [:a 1 :b 2 .c 3 :d 4])
+(index {:a 1 :b 2 .c 3 :d 4})
+(index #{:a 1 :b 2 :c 3 :d 4})
+
+(defn pos [e coll]
+  (for [[i v] (index coll) :whn (= e v)] i))
+
+(defn pos [pred coll]
+  (for [[i v] (index coll) :when (pred v)] i))
+
+;;functional programming techniques
+;;structural sharing
+(def baselist (list :barnabas :adam))
+(def lst1 (cons :willie baselist))
+(def lst2 (cons :phoeninx baselist))
+lst1
+lst2
+(= (next lst1) (next lst2))
+(identical? (next lst1) (next lst2))
+
+(defn xconj [t v]
+  (cond
+        (nil? t) {:val v :L nil :R nil}
+        (< v (:val t)) {:val (:val t)
+                        :L (xconj (:L t) v)
+                        :R (:R t)}
+        :else {:val (:val t),
+               :L (:L t),
+               :R (xconj (:R t) v)}))
+
+(def tree1 (xconj nil 5))
+tree1
+(def tree1 (xconj tree1 3))
+tree1
+(def tree1 (xconj tree1 2))
+
+(defn xseq [t]
+  (when t
+    (concat (xseq (:L t)) [(:val t)] (xseq (:R t)))))
+(xseq tree1)
+
+(def tree2 (xconj tree1 7))
+(xseq tree2)
+(identical? (:L tree1) (:L tree2))
+
+;; lazyness go to page 127
