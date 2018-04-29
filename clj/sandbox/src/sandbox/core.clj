1f148883cf75cf70d17da690a88ef20d1b2f64a9
Carlos Duque <carlosduque.hn@gmail.com>
2018-04-29 01:40:42 -0300

    added A* pathfinding example

--- clj/sandbox/src/sandbox/core.clj
+++ clj/sandbox/src/sandbox/core.clj
@@ -17,7 +17,7 @@
   (+ x y)
   [x y])
 
-;; locals 
+;; locals
 (let [r      5
       pi     3.1415
       r-squared (* r r)]
@@ -406,7 +406,7 @@ tree1
 
 (defn sort-parts [work]
   (lazy-seq
-            (loop [[part & parts] work] 
+            (loop [[part & parts] work]
               (if-let [[pivot & xs] (seq part)]
                 (let [smaller? #(< % pivot)]
                   (recur (list*
@@ -680,7 +680,7 @@ tree1
           :close (ff-closed r)
           :done true
           false))
-     (ff-closed [[_ & r]
+     (ff-closed [[_ & r]]
        "When the elevator is closed on the 1st floor
        it can either open or go up."
        #(case _
@@ -705,3 +705,79 @@ tree1
 
 (elevator [:close :open :close :up :open :open :done])
 (elevator [:close :up :open :close :down :open :done])
+
+;;A* pathfinding
+(def world [[  1   1   1   1   1]
+            [999 999 999 999   1]
+            [  1   1   1   1   1]
+            [  1 999 999 999 999]
+            [  1   1   1   1   1]])
+
+(neighbors 5 [0 0])
+
+(defn estimate-cost [step-cost-est size y x]
+    (* step-cost-est
+        (- (+ size size) y x 2)))
+
+(estimate-cost 900 5 0 0)
+(estimate-cost 900 5 4 4)
+
+(defn path-cost [node-cost cheapest-nbr]
+    (+ node-cost
+        (or (:cost cheapest-nbr) 0)))
+
+(path-cost 900 {:cost 1})
+
+(defn total-cost [newcost step-cost-est size y x]
+    (+ newcost
+        (estimate-cost step-cost-est size y x)))
+
+(total-cost 0 900 5 0 0)
+
+(total-cost (path-cost 900 {:cost 1}) 900 5 3 4)
+
+(defn min-by [f coll]
+    (when (seq coll)
+    (reduce (fn [min other]
+        (if (> (f min) (f other))
+            other
+            min))
+        coll)))
+
+(min-by :cost [{:cost 100} {:cost 36} {:cost 9}])
+
+(defn astar [start-yx step-est cell-costs]
+    (let [size (count cell-costs)]
+    (loop [steps 0
+        routes (vec (replicate size (vec (replicate size nil))))
+        work-todo (sorted-set [0 start-yx])]
+        (if (empty? work-todo)
+        [(peek (peek routes)) :steps steps]
+        (let [[_ yx :as work-item] (first work-todo)
+        rest-work-todo (disj work-todo work-item)
+        nbr-yxs (neighbors size yx)
+        cheapest-nbr (min-by :cost
+            (keep #(get-in routes %)
+            nbr-yxs))
+        newcost (path-cost (get-in cell-costs yx)
+            cheapest-nbr)
+        oldcost (:cost (get-in routes yx))]
+    (if (and oldcost (>= newcost oldcost))
+        (recur (inc steps) routes rest-work-todo)
+        (recur (inc steps)
+        (assoc-in routes yx
+            {:cost newcost
+             :yxs (conj (:yxs cheapest-nbr [])
+                yx)})
+        (into rest-work-todo
+            (map
+                (fn [w]
+                    (let [[y x] w]
+                        [(total-cost newcost step-est size y x) w]))
+                nbr-yxs))))))))))
+
+(astar [0 0]
+    900
+    world)
+
+;;ch8: macros
