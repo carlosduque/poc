21d551a4382c746b2eccb21d64768a48ea958485
Carlos Duque <carlosduque.hn@gmail.com>
2018-08-06 17:29:23 -0400

    added more routes

--- clj/hello-world-web/src/hello_world_web/handler.clj
+++ clj/hello-world-web/src/hello_world_web/handler.clj
@@ -4,13 +4,18 @@
             [ring.middleware.defaults :refer [wrap-defaults site-defaults]]))
 
 (defroutes app-routes
-  (GET "/" [] "Hello World")
-  (GET "/user/:number{[0-9]+}" [number]
-       (str "<h1>Hello World: " number "</h1>"))
-  ;;curl http://localhost:3000/user/carlos?greeting=hola
-  (GET "/user/:name" [name greeting]
-       (str "<h1>" greeting " " name "</h1>"))
-  (GET "/whats-my-ip" [request]
+  (GET "/" [r :as request] (str "Hello World, your request: " request))
+  (context "/user/:user-id" [user-id]
+    (GET "/:number{[0-9]+}" [number]
+        (str "<h1>Hello <em>" user-id "</em> your number is: " number "</h1>"))
+    ;;curl http://localhost:3000/user/carlos?greeting=hola
+    (GET "/:alias" [alias greeting]
+        {:status 200
+          :headers {"Content-Type" "text/html; charset=utf-8"}
+          :body (str "<h2>" greeting " @" alias ":<em>" user-id "</em></h2>")}))
+  (GET "/destructure" [x y & z :as r]
+       (str "x: " x ", y: " y ", z: " z " |full: " r))
+  (GET "/whats-my-ip" [r :as request]
        {:status 200
         :headers {"Content-Type" "text/plain"}
         :body (:remote-addr request)})
