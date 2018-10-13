8e20d2cbced66ba18a5d5276b102971ae0737b1d
Carlos Duque <carlosduque.hn@gmail.com>
2018-10-13 18:26:40 -0300

    added chapter 4 code

---
+++ clj/book-code/src/cbt/monsters.clj
@@ -0,0 +1,28 @@
+(ns cbt.monsters
+  (:gen-class))
+
+(def human-consumption [8.1 7.3 6.6 5.0])
+(def critter-consumption [0.0 0.2 0.3 1.1])
+
+(defn unify-diet-data
+  [human critter]
+  {:human human
+   :critter critter})
+
+(def sum #(reduce + %))
+(def avg #(/ (sum %) (count %)))
+(defn stats
+  [numbers]
+  (map #(% numbers) [sum count avg]))
+
+(stats [3 4 10])
+
+(def identities
+  [{:alias "Batman" :real "Bruce Wayne"}
+   {:alias "Spider-Man" :real "Peter Parker"}
+   {:alias "Santa" :real "Your mom"}
+   {:alias "Easter Bunny" :real "Your dad"}])
+
+(map :real identities)
+
+;continue on "reduce" section
