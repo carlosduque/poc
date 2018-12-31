84c536a366ae541b9e2721b5e70ae6fdbad819a0
Carlos Duque <carlosduque.hn@gmail.com>
2018-12-31 13:18:30 -0300

    web development with clojure 2ed: picture gallery initial commit

---
+++ clj/picture-gallery/src/clj/picture_gallery/handler.clj
@@ -0,0 +1,28 @@
+(ns picture-gallery.handler
+  (:require [picture-gallery.middleware :as middleware]
+            [picture-gallery.layout :refer [error-page]]
+            [picture-gallery.routes.home :refer [home-routes]]
+            [picture-gallery.routes.services :refer [service-routes]]
+            [compojure.core :refer [routes wrap-routes]]
+            [ring.util.http-response :as response]
+            [compojure.route :as route]
+            [picture-gallery.env :refer [defaults]]
+            [mount.core :as mount]))
+
+(mount/defstate init-app
+  :start ((or (:init defaults) identity))
+  :stop  ((or (:stop defaults) identity)))
+
+(mount/defstate app
+  :start
+  (middleware/wrap-base
+    (routes
+      (-> #'home-routes
+          (wrap-routes middleware/wrap-csrf)
+          (wrap-routes middleware/wrap-formats))
+          #'service-routes
+      (route/not-found
+        (:body
+          (error-page {:status 404
+                       :title "page not found"}))))))
+
