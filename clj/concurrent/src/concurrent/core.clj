bbba0b74d454b78e8aecdc83452aaef2f1213ba7
Carlos Duque <carlosduque.hn@gmail.com>
2019-07-23 18:15:22 -0400

    added macro to create threads

--- clj/concurrent/src/concurrent/core.clj
+++ clj/concurrent/src/concurrent/core.clj
@@ -1,11 +1,24 @@
 (ns concurrent.core
+  (:import java.security.MessageDigest
+           java.math.BigInteger)
   (:gen-class))
+
+(defmacro with-new-thread [& body]
+  `(.start (Thread. (fn [] ~@body))))
 
 (defn now []
   (str (java.time.Instant/now)))
 
 (defn gen-uuid []
   (str (java.util.UUID/randomUUID)))
+
+(defn md5
+  [^String s]
+  (->> s
+       .getBytes
+       (.digest (MessageDigest/getInstance "MD5"))
+       (BigInteger. 1)
+       (format "%032x")))
 
 (defn complex-job
   [id sent-at]
@@ -18,9 +31,24 @@
      :end   (now)
      :duration duration}))
 
+(def url "http://www.gutenberg.org/cache/epub/103/pg103.txt")
+
+(defn get-document
+  [url]
+  (let [url url
+        id (md5 url)]
+    {:id id
+     :url url
+    :mime "text/plain"
+    :content (delay (slurp url))}))
+
 (defn deref-val
   [value]
   (println "value" @value))
+
+(with-new-thread (get-document url))
+
+(with-new-thread (complex-job gen-uuid now))
 
 (def thread (Thread. (complex-job gen-uuid now)))
 (.start thread)
