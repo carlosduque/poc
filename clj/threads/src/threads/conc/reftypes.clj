98f60840c514a7c0e177f8f110dc2177ebee6b97
Carlos Duque <carlosduque.hn@gmail.com>
2019-07-25 17:02:17 -0400

    added reftypes

---
+++ clj/threads/src/threads/conc/reftypes.clj
@@ -0,0 +1,6 @@
+(ns threads.conc.reftypes
+  (:require [threads.util.core :as u])
+  (:gen-class))
+
+;;State: clojure's immutable datastructures
+;;Identity: clojure's ref types (vars, atoms, refs, agents)
