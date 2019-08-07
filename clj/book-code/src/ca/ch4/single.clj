dc098240c85db42c21b7a90b4046949739587a01
Carlos Duque <carlosduque.hn@gmail.com>
2019-08-07 14:57:46 -0400

    added ch4 missing code

--- clj/book-code/src/ca/ch4/single.clj
+++ clj/book-code/src/ca/ch4/single.clj
@@ -9,3 +9,15 @@
       (recur items (conj cart item))
       cart)))
 
+(defn shop-for-item [cart item]
+  "Shop for an item, return updated cart"
+  (if (store/grab item)
+    (conj cart item)
+    cart))
+
+(defn go-shopping
+  "Returns a list of items purchased"
+  [shopping-list]
+  (reduce shop-for-item [] shopping-list))
+
+
--- clj/book-code/src/ca/ch4/store.clj
+++ clj/book-code/src/ca/ch4/store.clj
@@ -1,5 +1,7 @@
 (ns ca.ch4.store)
 
+#_(def inventory (atom {} :validator no-negative-values?))
+
 (def inventory (atom {}))
 
 (defn no-negative-values?
@@ -29,4 +31,29 @@
   "stock an item on the shelves"
   [item]
   (swap! inventory update-in [item] inc))
+
+(declare sold-items)
+
+(defn restock-order
+  "a watch to restock an item"
+  [k r ov nv]
+  (doseq [item (for [kw (keys ov)
+                     :when (not= (kw ov) (kw nv))] kw)]
+    (swap! sold-items update-in [item] (fnil inc 0))
+    (println "need to restock" item)))
+
+(defn init-with-restock
+  "set up store with inventory"
+  [m]
+  (def inventory (atom m))
+  (def sold-items (atom {}))
+  (set-validator! inventory no-negative-values?)
+  (add-watch inventory :restock restock-order))
+
+(defn restock-all
+  "restock all items sold"
+  []
+  (swap! inventory #(merge-with + % @sold-items))
+  (reset! sold-items {})) ;;dragons here
+
 
