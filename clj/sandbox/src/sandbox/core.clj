38bc401b5161ad75f581a13ad039f8ace58bcbf6
Carlos Duque <carlosduque.hn@gmail.com>
2019-08-13 19:45:59 -0400

    added 4clojure solutions for #30 and #31

--- clj/sandbox/src/sandbox/core.clj
+++ clj/sandbox/src/sandbox/core.clj
@@ -1,7 +1,16 @@
 (ns sandbox.core
   (:gen-class))
 
-(defn -main
-  "I don't do a whole lot ... yet."
-  [& args]
-  (println "Hello, World!"))
+;; #30
+(def mystr "Leeeerrroy")
+(def myvec [1 1 2 3 3 2 2 3])
+(def mytup [[1 2] [1 2] [3 4] [1 2]])
+
+(#(reduce (fn [acc x]
+           (if (not= (last acc) x)
+              (conj acc x)
+              acc))
+         [] %) mystr)
+
+;; #31
+#(partition-by identity %)
