fcf3c365d402937575fadd2b5efafe93680d2480
Carlos Duque <carlosduque.hn@gmail.com>
2019-08-13 22:59:23 -0400

    #28-wip

--- clj/sandbox/src/sandbox/core.clj
+++ clj/sandbox/src/sandbox/core.clj
@@ -1,6 +1,21 @@
 (ns sandbox.core
   (:gen-class))
 
+;; #28
+(def nested1 '((1 2) 3 [4 [5 6]]))
+(def nested2 ["a" ["b"] "c"])
+(def nested3 '((((:a)))))
+
+(defn lat
+  [coll]
+  (loop [acc '()
+         xs coll]
+    (cond
+      (nil? xs) acc
+      (seqable? (first xs)) (recur acc (first xs))
+      :else (recur (conj acc (first xs)) (rest xs)))))
+
+
 ;; #30
 (def mystr "Leeeerrroy")
 (def myvec [1 1 2 3 3 2 2 3])
